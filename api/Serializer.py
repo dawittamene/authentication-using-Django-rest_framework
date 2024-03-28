a015a12892e536ec5aaf2ce8944cae7e685bab23
dawittamene <dawittamene559@gmail.com>
2024-03-28 14:19:45 +0300

    done serializer

--- api/Serializer.py
+++ api/Serializer.py
@@ -11,3 +11,42 @@ class ProfileSerializer(serializers.MOdelSerializer):
     class Meta:
         model = Profile
         fields = ['id','user','full_name','bio','image','verified']        
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls,user):
+        token=super().get_token(user)
+        
+        
+        token['full_name'] = user.profile.full_name
+        token['username'] = user.username
+        token['email'] = user.email
+        token['bio'] = user.profile.bio
+        token['image'] = user.profile.image
+        token['verified'] = user.profile.verified
+        
+        return token
+
+class ReigsterSerialzer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])   
+    confirm_password = serializers.CharField(write_only=True, required=True)   
+    
+    class Meta:
+        model = User
+        fields = ['email','username', 'password', 'confirm_password']
+        
+        
+        def validate(self, attrs):
+            if attrs ['password']!=attrs['confirm_password']:
+                raise serializers.ValidationError(
+                    {'password':'password fields does not match'}
+                )
+            return attrs
+        def create(self, validated_data):
+            user = User.objects.create(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                
+            )    
+            user.set_password(validated_data['password'])
+            user.save()
+            return user
--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_frame_work.decorators import api_view
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
 
-# Create your views here.
+from api.models import *
+from api.Serializer import *
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
